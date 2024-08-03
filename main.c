31f27b3a0af5d802af998554de78efff18fb5446
YassinBaraa <baraayassin05@gmail.com>
2024-08-03 17:03:58 +0200

    added grid

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 #define ROWS 10
-#define COLS 10
+#define COLS 20
 
 int main()
 {
+    int grid[ROWS][COLS] = {0};
 
-    return 0;
+    printf("======= TETRIS=======\n");
+    for (int i = 0; i < COLS; ++i)
+    {
+        for (int j = 0; j < ROWS; ++j)
+        {
+            printf("%d ", grid[j][i]);
+            /*if (grid[j][i] == 0)
+            {
+                printf("%c", 254);
+            }*/
+        }
+        printf("\n");
+    }
 }
